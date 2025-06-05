ac2f84d4fd59a53146b1ecc0f3f96f99aed69f5f
Herbertia <117363174+CaracaraEXE@users.noreply.github.com>
2025-06-05 18:59:05 -0300

    added a quick test

--- src/components/HomeBody.js
+++ src/components/HomeBody.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import MovieGrid from './MovieGrid'
+import Link from 'next/link'
 
 const HomeBody = () => {
   return (
     <div>
+      <p>Estuve enferma toda la semana :c</p>
+      <Link href={"/movie/1100988"}>Test rapido de peliculas</Link>
       <h2>Recent Films</h2>
       <MovieGrid/>
     </div>
