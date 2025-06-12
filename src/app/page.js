fa21e5d7558de39ec441d06a59afbf2786829d5c
Herbertia <117363174+CaracaraEXE@users.noreply.github.com>
2025-06-12 19:51:07 -0300

    ASIDE ADDEDDDDD

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,52 @@
-import Image from "next/image";
+'use client'
+
+import {useState, useEffect} from 'react';
 import HomeBody from "@/components/HomeBody.js"
+import FeaturedMovie from '@/components/FeaturedMovie';
+import LoadingIcon from '@/components/LoadingIcon';
+import axios from 'axios';
 
 export default function Home() {
+  const API_URL = 'https://api.themoviedb.org/3/trending/movie/day?api_key=8d155a452063365b70d7e38e2609b662';
+
+  const [movies,setMovies] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+useEffect(() => {
+const getData = async () => {
+  setLoading(true);
+  try {
+    const response = await axios.get(API_URL);
+    const moviesData = response.data.results
+    setMovies(moviesData);
+    setLoading(false);
+    console.log(moviesData);
+  } catch(error) {
+    console.log("No se han podido mostrar las mubis.")
+    setError(true);
+  }
+}
+
+getData();
+
+},[]);
+
   return (
-    <div className="flex mx-auto w-fit">
-    <HomeBody/>
-    </div>
+    <>
+    {!loading &&
+        <div className="flex mx-auto w-fit">
+        <aside className='bg-gray-400 px-4 border-black border-x-4 w-[15em]'>
+          <h2 className='text-2xl mt-5 border-y-2'>&#10022; Featured Film &#10022;</h2>
+          <FeaturedMovie movies={movies}/>
+        </aside>
+        <HomeBody movies={movies}/>
+        </div>
+    }
+
+    {loading && <LoadingIcon/>}
+
+        {error && "HUBO UN ERROR"}
+    </>
   );
 }
--- src/components/HomeBody.js
+++ src/components/HomeBody.js
@@ -1,49 +1,15 @@
-'use client'
 
-import {useState, useEffect} from 'react';
 
-import Link from 'next/link'
 import React from 'react'
-import axios from 'axios';
 import MovieGrid from './MovieGrid';
-import LoadingIcon from './LoadingIcon';
-import FeaturedMovie from './FeaturedMovie';
 import Image from 'next/image';
 
-const HomeBody = () => {
+const HomeBody = ({movies}) => {
 
-  const API_URL = 'https://api.themoviedb.org/3/trending/movie/day?api_key=8d155a452063365b70d7e38e2609b662';
-
-  const [movies,setMovies] = useState([]);
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(true);
-
-useEffect(() => {
-const getData = async () => {
-  setLoading(true);
-  try {
-    const response = await axios.get(API_URL);
-    const moviesData = response.data.results
-    setMovies(moviesData);
-    setLoading(false);
-    console.log(moviesData);
-  } catch(error) {
-    console.log("No se han podido mostrar las mubis.")
-    setError(true);
-  }
-}
-
-getData();
-
-},[]);
-
+  
   //BRANCHES ARE STOOPID
   return (
-    <div className='home_body lg:w-[50em] text-center'>
-      {!loading &&
-
-        <>
-
+    <div className='home_body lg:w-[40em] text-center'>
         <p className='italic text-center p-5'>Testing yo' ass</p>
 
         <Image
@@ -53,15 +19,9 @@ getData();
           height={400}
           className='mx-auto'/>
 
-        <h2 className='text-3xl mt-5 border-y-2'>&#10022; Featured Film &#10022;</h2>
-        <FeaturedMovie movies={movies} key={"featured_movie"}/>
         <h2 className='text-3xl border-y-2'>&#10022; Recent Films &#10022;</h2>
         <MovieGrid movies={movies} key={"moviegrid"}/>
-        </>
-      }
-      {loading && <LoadingIcon/>}
-
-        {error && "HUBO UN ERROR"}
+      
     </div>
   )
 }
