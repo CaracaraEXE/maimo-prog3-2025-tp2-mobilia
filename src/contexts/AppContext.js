eed17ffe40eef05e935aba9ed9969b815c9cd808
Herbertia <117363174+CaracaraEXE@users.noreply.github.com>
2025-06-13 22:37:18 -0300

    prev class fixes

--- src/contexts/AppContext.js
+++ src/contexts/AppContext.js
@@ -9,7 +9,7 @@ export const AppContextProvider = ({children}) => {
     return(
         <AppContext.Provider
             value={
-                {favorites}
+                {favorites,}
             }
         >
             {children}
